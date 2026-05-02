e9b89c5dfa5fee939b234751dd08f7db2725d7e8
vedant-chautiwar <chautiwarvedant@gmail.com>
2026-05-02 14:34:05 +0530

    update code

--- api/server.js
+++ api/server.js
@@ -8,11 +8,12 @@ import releaseHandler from "./release.js";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
+const publicDir = path.join(__dirname, "..", "public");
 const app = express();
 const preferredPort = Number(process.env.PORT || 3000);
 
 app.use(express.json());
-app.use(express.static(__dirname));
+app.use(express.static(publicDir));
 
 function runHandler(handler) {
   return (req, res) => handler(req, res);
@@ -28,7 +29,7 @@ app.all("/api/slots/:id/release", (req, res) => {
 });
 
 app.get("*", (_req, res) => {
-  res.sendFile(path.join(__dirname, "index.html"));
+  res.sendFile(path.join(publicDir, "index.html"));
 });
 
 function startServer(port, attemptsLeft = 10) {
